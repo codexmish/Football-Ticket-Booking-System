a4f04de584cd22832b8582af3766f186620a0ef4
codexmish <majharulislam.shanto.70@gmail.com>
2026-06-14 12:12:48 +0600

    query 1 done

--- QUERY.sql
+++ QUERY.sql
@@ -49,3 +49,11 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (503, 2, 101, 'A-13', 'Confirmed', 150.00),
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
+
+
+
+
+
+--query 1
+select match_id, fixture, base_ticket_price from matches
+where tournament_category = 'Champions League' and match_status = 'Available'
